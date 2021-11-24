494b9ee6cb21c0bbf6e966ae422335e4ac8185aa
kamyasimon <crankluga@gmail.com>
2021-11-24 21:37:45 +0300

    >>Start of the Lazarus. >Intsalled the basic components >Vue Router >Bootstrap Vue

---
+++ resources/js/pages/Home.vue
@@ -0,0 +1,22 @@
+<template>
+   <div class="container-fluid">
+       <div class="row">
+           <nav class="nav nav-fill">
+               <li class="nav-item">
+                   <a class="nav-link active" href="#">Home</a>
+               </li>
+               <li class="nav-item">
+                   <a class="nav-link disabled" href="#" tabindex="-1" aria-disabled="true">Item 2</a>
+               </li>
+           </nav>
+       </div>
+   </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+            console.log('Component mounted.')
+        }
+    }
+</script>
